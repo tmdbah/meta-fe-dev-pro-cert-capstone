cee4c275b124b12955d0e0fde1fbc6609662abb0
TJ Bah <tmdbah@gmail.com>
2023-09-15 00:38:36 -0400

    navBar updates, grid mobile 1st refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import foodPlatter from "../assets/img/reserve-table.jpg";
+import "../App.css";
 
 function Header() {
   return (
@@ -6,12 +7,10 @@ function Header() {
       <h1 className="header-title">Little Lemon</h1>
       <h2 className="header-subTitle">Chicago</h2>
       <p className="header-description">
-        Donec interdum, metus et hendrerit aliquet, dolor diam sagittis ligula,
-        eget egestas libero turpis vel mi. Nulla porta dolor. Etiam iaculis nunc
-        ac metus. Etiam rhoncus. Quisque rutrum. Etiam feugiat lorem non metus.
-        Aenean tellus metus.
+        We are a family owned Mediterranean restaurant, focused on traditional
+        recipes served with a modern twist.
       </p>
-      <button className="header-reserveTableButton">Reserve a Table</button>
+      <button className="header-reserveTableButton">Reserve a table</button>
       <img
         className="header-headerImg"
         height={350}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
+import "../App.css";
+
 function Main() {
   return (
     <main>
-      <div className="main-container">
+      {/* <div className="main-container">
         <h2 className="main-title">This weeks specials!</h2>
         <button className="main-online-menu">Online Menu</button>
-      </div>
+      </div> */}
     </main>
   );
 }
