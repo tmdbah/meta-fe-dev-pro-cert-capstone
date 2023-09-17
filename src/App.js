cf9ae8b40737f53f520964db52e526439b6da661
TJ Bah <tmdbah@gmail.com>
2023-09-17 00:01:22 -0400

    Updated components and installed react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Header />
-      <Main />
-      <Footer />
+      <Homepage />
     </>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import "../App.css";
+import Navbar from "./Navbar";
+import CallToAction from "./CallToAction";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function Homepage() {
+  return (
+    <>
+      <Navbar />
+      <CallToAction />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Homepage;
