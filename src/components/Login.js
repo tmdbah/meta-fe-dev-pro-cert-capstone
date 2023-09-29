5f559eb11c9da5909f13d4daf41e8ca3321d4627
TJ Bah <tmdbah@gmail.com>
2023-09-28 22:29:23 -0400

    Added form component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,14 @@
 import "../App.css";
 
 function Login() {
+  const myList = ["apple", "banana", "orange"];
+
+  const listItems = myList.map((item) => <li key={item}>{item}</li>);
+
   return (
     <>
       <h1 className="center">Login</h1>
+      <ul className="center">{listItems}</ul>
     </>
   );
 }
--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,9 +1,11 @@
 import "../App.css";
+import BookingForm from "./BookingForm";
 
 function Reservations() {
   return (
     <>
       <h1 className="center">Reservations</h1>
+      <BookingForm />
     </>
   );
 }
